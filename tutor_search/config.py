224b4848996cee69a12d4d411d2924c9c863a825
quyencanh203 <quyencanh203@gmail.com>
2024-03-30 17:46:05 +0700

    update requiments.txt

--- tutor_search/config.py
+++
@@ -1,9 +0,0 @@
-# Cấu hình ứng dụng (DATABASE_URI, SECRET_KEY, ...)
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
